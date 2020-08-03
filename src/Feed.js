d4e1f3870727fcd3f79fd9908d605d25f50221bc
James Moriarty <jamespaulmoriarty@gmail.com>
2020-08-03 21:39:53 +1000

    initial commit

---
+++ src/Feed.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import InstagramFeed from './InstagramFeed'
+import Media from './Media';
+import './Feed.css';
+
+class Feed extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { media: [] };
+
+    InstagramFeed.Get(props.username).then(media => this.setState({ media: media }))
+  }
+
+  render() {
+    return (
+      <div className="Feed">
+        {this.state.media.map((media, index) => (
+          <Media key={index} src={media.src} url={media.url} alt={media.alt} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Feed;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Feed from './Feed';
+import './index.css';
+
+ReactDOM.render(
+  <Feed username="jamespaulmoriarty" />,
+  document.getElementById('root')
+);
